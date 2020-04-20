effaf270c03d032a47999daffa634c18ceb476cd
lucas_peron <lucas_peron@outlook.com.ar>
2020-04-19 21:32:56 -0300

    19/04/2020 9:32pm

---
+++ Controllers/ConceptosBasic3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.website.Controllers
+{
+    public class ConceptosBasic3Controller : Controller
+    {
+        // GET: ConceptosBasic3
+        public ActionResult ConceptosBásicos3()
+        {
+            return View();
+        }
+    }
+}
